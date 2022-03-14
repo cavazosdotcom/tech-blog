08a3068080e17eb3b7355319df90cea171841398
cavazosdotcom <cavazosdotcom@gmail.com>
2022-03-14 15:21:33 -0700

    added post route for posts, basic homepage with posts shown

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -17,5 +17,21 @@ router.get('/', async (req, res) => {
   }
 });
 
+// router.post('/new', withAuth, (req, res) => {
+//   res.render('create-post')
+// });
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newPost = await Post.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newPost);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 
 module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -28,6 +28,31 @@ router.get('/', withAuth, async (req, res) => {
   }
 });
 
+
+
+router.get('/posts', withAuth, async (req, res) => {
+  try {
+  const postData = await Post.findAll({
+    include: [
+      {
+        model: User,
+        attributes: ['name'],
+      },
+    ],
+  });
+  const posts = postData.map((post) => post.get({ plain: true }));
+  console.log(posts)
+  res.render('create-post', { 
+    posts, 
+    logged_in: req.session.logged_in 
+  });
+} catch (err) {
+  res.status(500).json(err);
+}
+});
+
+
+
 // router.get('/post/:id', async (req, res) => {
 //   try {
 //     const postData = await Post.findByPk(req.params.id, {
